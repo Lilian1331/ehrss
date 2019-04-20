3f19191cafa93c72ba85e5cd318fb814a13e5f59
lilian1331 <lilian-1331@hotmail.com>
2019-04-20 17:44:05 +0800

    dev

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ global.document = document;
 app.use(routes);
 app.use(express.static( "public" ));
 app.use(express.static( "node_modules" ));
+app.use(function(req,res,next){
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers','Content-Type');
+})
 
 
 // parse application/x-www-form-urlencoded
